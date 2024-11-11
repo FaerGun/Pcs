bfd6a1a5ecb19ac1164059a418b1bf584c1ee780
FaerGun <shved03@bk.ru>
2024-11-11 16:00:35 +0300

    8

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,12 @@
-import 'page/favorite.dart';
-import 'page/profile.dart';
 import 'package:flutter/material.dart';
-import 'models/note.dart';
-import 'page/home.dart';
+import 'product_list_screen.dart';
+import 'profile_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     Color swampColor = Color(0xFF4B8B3B);
@@ -20,85 +16,50 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
-  Set<Gear> favoriteGears = <Gear>{};
-
-  static const List<Widget> _widgetTitles = [
-    Text('Главная'),
-    Text('Избранное'),
-    Text('Профиль'),
-  ];
-
-  late List<Widget> _widgetOptions;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _widgetOptions = <Widget>[
-      HomePage(
-        favoriteGears: favoriteGears,
-        onFavoriteChanged: _onFavoriteChanged,
-      ),
-      FavoritePage(
-        favoriteGears: favoriteGears,
-        onFavoriteChanged: _onFavoriteChanged,
-      ),
-      const ProfilePage(),
-    ];
-  }
-
-  void _onFavoriteChanged(Gear gear, bool isFavorite) {
-    setState(() {
-      if (isFavorite) {
-        favoriteGears.add(gear);
-      } else {
-        favoriteGears.remove(gear);
-      }
-    });
-  }
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _widgetOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Главная',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Избранное',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Профиль',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: const Color.fromARGB(255, 32, 100, 156),
-        onTap: _onItemTapped,
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProductListScreen(),
+                  ),
+                );
+              },
+              child: Text('View Products'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProfileScreen(
+                      fullName: 'Jared Leto',
+                      phoneNumber: '+1228337890',
+                      email: 'leto.doe@example.com',
+                    ),
+                  ),
+                );
+              },
+              child: Text('Profile'),
+            ),
+          ],
+        ),
       ),
     );
   }
